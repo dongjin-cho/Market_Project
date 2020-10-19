ec9fee56798eff55fdd2f9fe2566b93f88d4b2a4
dongjin cho <dongjin_fc@dongjinui-MacBookPro.local>
2020-10-19 17:45:37 +0900

    rest api 끝

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,10 +1,452 @@
 const db = require('../../models');
 const models = require('../../models');
 
+
+// future coonect
+// customers table
+exports.get_customers = ( _ , res) => {
+    models.customers.findAll({
+    }).then((customerList)=>{
+        res.json(customerList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_customers = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.customers.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_customers_edit = (req, res) => {
+    models.customers.findByPk(req.params.id).then((customer)=>{
+        res.json(customer);
+    })
+}
+
+exports.post_customers_edit = (req, res) =>{
+    models.customers.update({
+        //data
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        birthdate: req.body.birthdate,
+        phone: req.body.phone,
+        address: req.body.address,
+        createdate: req.body.createdate
+    },{
+        //condition
+        where: {customer_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// carts table
+exports.get_carts = ( _ , res) => {
+    models.carts.findAll({
+    }).then((cartList)=>{
+        res.json(cartList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_carts = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.carts.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_carts_edit = (req, res) => {
+    models.carts.findByPk(req.params.id).then((cart)=>{
+        res.json(cart);
+    })
+}
+
+exports.post_carts_edit = (req, res) =>{
+    models.carts.update({
+        //data
+        customer_id: req.body.customer_id,
+        amount_price: req.body.amount_price,
+        cart_status: req.body.cart_status,
+    },{
+        //condition
+        where: {cart_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// cart_list table
+exports.get_cart_lists = ( _ , res) => {
+    models.cart_lists.findAll({
+    }).then((cart_listList)=>{
+        res.json(cart_listList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_cart_lists = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.cart_lists.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_cart_lists_edit = (req, res) => {
+    models.cart_lists.findByPk(req.params.id).then((cart_list)=>{
+        res.json(cart_list);
+    })
+}
+
+exports.post_cart_lists_edit = (req, res) =>{
+    models.cart_lists.update({
+        //data
+        cart_id: req.body.cart_id,
+        cart_item_id: req.body.cart_item_id,
+    },{
+        //condition
+        where: {cart_list_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// cart_items table
+exports.get_cart_items = ( _ , res) => {
+    models.cart_items.findAll({
+    }).then((cart_itemList)=>{
+        res.json(cart_itemList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_cart_items = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.cart_items.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_cart_items_edit = (req, res) => {
+    models.cart_items.findByPk(req.params.id).then((cart_item)=>{
+        res.json(cart_item);
+    })
+}
+
+exports.post_cart_items_edit = (req, res) =>{
+    models.cart_items.update({
+        //data
+        product_id: req.body.product_id,
+        quantity: req.body.quantity,
+        total_price: req.body.total_price,
+    },{
+        //condition
+        where: {cart_item_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// purchases table
+exports.get_purchases = ( _ , res) => {
+    models.purchases.findAll({
+    }).then((purchaseList)=>{
+        res.json(purchaseList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_purchases = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.purchases.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_purchases_edit = (req, res) => {
+    models.purchases.findByPk(req.params.id).then((purchase)=>{
+        res.json(purchase);
+    })
+}
+
+exports.post_purchases_edit = (req, res) =>{
+    models.purchases.update({
+        //data
+        cart_id: req.body.cart_id,
+        customer_id: req.body.customer_id,
+        delivery_address: req.body.delivery_address,
+        delivery_option: req.body.delivery_option,
+        purchase_status: req.body.purchase_status,
+        order_timestamp: req.body.order_timestamp,
+    },{
+        //condition
+        where: {purchase_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// products table
 exports.get_products = ( _ , res) => {
-    // res.render( 'admin/products.html' , 
-    //     { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
-    // );
+    models.products.findAll({
+    }).then((productList)=>{
+        res.json(productList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_products = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.products.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_products_edit = (req, res) => {
+    models.products.findByPk(req.params.id).then((product)=>{
+        res.json(product);
+    })
+}
+
+exports.post_products_edit = (req, res) =>{
+    models.products.update({
+        //data
+        category: req.body.category,
+        name: req.body.name,
+        provider_id: req.body.provider_id,
+        retail_price: req.body.retail_price,
+        location: req.body.location,
+        description: req.body.description,
+        img_path: req.body.img_path,
+    },{
+        //condition
+        where: {product_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// notification_infos table
+exports.get_notification_infos = ( _ , res) => {
+    models.notification_infos.findAll({
+    }).then((notification_infoList)=>{
+        res.json(notification_infoList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_notification_infos = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.notification_infos.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_notification_infos_edit = (req, res) => {
+    models.notification_infos.findByPk(req.params.id).then((notification_info)=>{
+        res.json(notification_info);
+    })
+}
+
+exports.post_notification_infos_edit = (req, res) =>{
+    models.notification_infos.update({
+        //data
+        product_id: req.body.product_id,
+        attr1: req.body.attr1,
+        attr2: req.body.attr2,
+    },{
+        //condition
+        where: {notification_info_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// hashtag_lists table
+exports.get_hashtag_lists = ( _ , res) => {
+    models.hashtag_lists.findAll({
+    }).then((hashtag_listList)=>{
+        res.json(hashtag_listList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_hashtag_lists = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.hashtag_lists.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_hashtag_lists_edit = (req, res) => {
+    models.hashtag_lists.findByPk(req.params.id).then((hashtag_list)=>{
+        res.json(hashtag_list);
+    })
+}
+
+exports.post_hashtag_lists_edit = (req, res) =>{
+    models.hashtag_lists.update({
+        //data
+        product_id: req.body.product_id,
+        hashtag_id: req.body.hashtag_id,
+    },{
+        //condition
+        where: {hashtag_list_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// hashtags table
+exports.get_hashtags = ( _ , res) => {
+    models.hashtags.findAll({
+    }).then((hashtagList)=>{
+        res.json(hashtagList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_hashtags = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.hashtags.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_hashtags_edit = (req, res) => {
+    models.hashtags.findByPk(req.params.id).then((hashtag)=>{
+        res.json(hashtag);
+    })
+}
+
+exports.post_hashtags_edit = (req, res) =>{
+    models.hashtags.update({
+        //data
+        hashtag: req.body.hashtag,
+    },{
+        //condition
+        where: {hashtag_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// product_orders table
+exports.get_product_orders = ( _ , res) => {
+    models.product_orders.findAll({
+    }).then((product_orderList)=>{
+        res.json(product_orderList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_product_orders = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.product_orders.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_product_orders_edit = (req, res) => {
+    models.product_orders.findByPk(req.params.id).then((product_order)=>{
+        res.json(product_order);
+    })
+}
+
+exports.post_product_orders_edit = (req, res) =>{
+    models.product_orders.update({
+        //data
+        provider_id: req.body.provider_id,
+        product_id: req.body.product_id,
+        quantity: req.body.quantity,
+        amount_price: req.body.amount_price,
+        order_timestamp: req.body.order_timestamp,
+        stock_timestamp: req.body.stock_timestamp,
+    },{
+        //condition
+        where: {product_order_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// providers table
+exports.get_providers = ( _ , res) => {
+    models.providers.findAll({
+    }).then((providerList)=>{
+        res.json(providerList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_providers = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.providers.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_providers_edit = (req, res) => {
+    models.providers.findByPk(req.params.id).then((provider)=>{
+        res.json(provider);
+    })
+}
+
+exports.post_providers_edit = (req, res) =>{
+    models.providers.update({
+        //data
+        phone: req.body.phone,
+        address: req.body.address,
+    },{
+        //condition
+        where: {provider_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// provider_handles table
+exports.get_provider_handles = ( _ , res) => {
+    models.provider_handles.findAll({
+    }).then((provider_handleList)=>{
+        res.json(provider_handleList);
+    }) // 이곳으로 productList보내기
+}
+
+exports.post_provider_handles = ( req , res ) => {
+    
+    console.log('body =' + req.body);
+    models.provider_handles.create(req.body).then( () => {
+        console.log('body =' +req.body);
+        res.json(req.body);
+    });   
+} 
+exports.get_provider_handles_edit = (req, res) => {
+    models.provider_handles.findByPk(req.params.id).then((provider_handle)=>{
+        res.json(provider_handle);
+    })
+}
+
+exports.post_provider_handles_edit = (req, res) =>{
+    models.provider_handles.update({
+        //data
+        provider_id: req.body.provider_id,
+        product_id: req.body.product_id,
+        prov_price: req.body.prov_price,
+    },{
+        //condition
+        where: {provider_handle_id: req.params.id}
+    }).then(()=>{
+        res.json(req.body);
+    })
+}
+
+// old
+/*
+exports.get_products = ( _ , res) => {
     models.Products.findAll({
 
     }).then((producs)=>{
@@ -70,76 +512,4 @@ exports.get_products_delete = (req, res) => {
     })
 }
 
-exports.get_customers = ( _ , res) => {
-    models.customers.findAll({
-    }).then((customerList)=>{
-        //console.log(customerList)
-        //res.send(customerList)
-        
-        //HTML Return
-        //res.render('admin/customers.html', {customers: customerList})
-
-        //JSON Return
-        res.json(customerList);
-    }) // 이곳으로 productList보내기
-}
-
-exports.post_customers = ( req , res ) => {
-    
-    console.log('body =' + req.body);
-    models.customers.create(req.body).then( () => {
-        console.log('body =' +req.body);
-        res.json(req.body);
-    });   
-} 
-exports.get_customers_edit = (req, res) => {
-    models.customers.findByPk(req.params.id).then((customer)=>{
-        res.json(customer);
-    })
-}
-
-exports.post_customers_edit = (req, res) =>{
-    models.customers.update({
-        //data
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-        birthdate: req.body.birthdate,
-        phone: req.body.phone,
-        address: req.body.address,
-        createdate: req.body.createdate
-    },{
-        //condition
-        where: {customer_id: req.params.id}
-    }).then(()=>{
-        res.json(req.body);
-    })
-}
-
-exports.get_products = ( _ , res) => {
-    // res.render( 'admin/products.html' , 
-    //     { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
-    // );
-    models.products.findAll({
-
-    }).then((products)=>{
-        res.render('admin/products.html', {products: products})
-    }) // 이곳으로 productList보내기
-}
-
-exports.get_orders = ( _ , res) => {
-    models.orders.findAll({
-    }).then((orderList)=>{
-        //console.log(orderList)
-        res.render('admin/order.html', {orders: orderList})
-    }) // 이곳으로 productList보내기
-}
-
-exports.get_orders_detail = ( req, res ) => {
-    models.order_details.findAll({
-        where: { order_id : req.params.id}
-    }).then( (order_details) => {
-        //res.send(product);
-        res.render('admin/order_detail.html', { order_details: order_details });  
-    });
-};
+*/
--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -16,8 +16,72 @@ router.get('/', testMiddleWare, testMiddleWare2 , (req,res) => {
     res.send('admin app');
 });
 
-//router.get('/products', ctrl.get_products );
+// future connect 
+router.get('/customers', ctrl.get_customers );
+router.post('/customers', ctrl.post_customers );
+router.get('/customers/:id', ctrl.get_customers_edit );
+router.post('/customers/:id', ctrl.post_customers_edit );
 
+router.get('/carts', ctrl.get_carts );
+router.post('/carts', ctrl.post_carts );
+router.get('/carts/:id', ctrl.get_carts_edit );
+router.post('/carts/:id', ctrl.post_carts_edit );
+
+router.get('/cart_lists', ctrl.get_cart_lists );
+router.post('/cart_lists', ctrl.post_cart_lists );
+router.get('/cart_lists/:id', ctrl.get_cart_lists_edit );
+router.post('/cart_lists/:id', ctrl.post_cart_lists_edit );
+
+router.get('/cart_items', ctrl.get_cart_items );
+router.post('/cart_items', ctrl.post_cart_items );
+router.get('/cart_items/:id', ctrl.get_cart_items_edit );
+router.post('/cart_items/:id', ctrl.post_cart_items_edit );
+
+router.get('/purchases', ctrl.get_purchases );
+router.post('/purchases', ctrl.post_purchases );
+router.get('/purchases/:id', ctrl.get_purchases_edit );
+router.post('/purchases/:id', ctrl.post_purchases_edit );
+
+router.get('/products', ctrl.get_products );
+router.post('/products', ctrl.post_products );
+router.get('/products/:id', ctrl.get_products_edit );
+router.post('/products/:id', ctrl.post_products_edit );
+
+router.get('/notification_infos', ctrl.get_notification_infos );
+router.post('/notification_infos', ctrl.post_notification_infos );
+router.get('/notification_infos/:id', ctrl.get_notification_infos_edit );
+router.post('/notification_infos/:id', ctrl.post_notification_infos_edit );
+
+router.get('/hashtag_lists', ctrl.get_hashtag_lists );
+router.post('/hashtag_lists', ctrl.post_hashtag_lists );
+router.get('/hashtag_lists/:id', ctrl.get_hashtag_lists_edit );
+router.post('/hashtag_lists/:id', ctrl.post_hashtag_lists_edit );
+
+router.get('/hashtags', ctrl.get_hashtags );
+router.post('/hashtags', ctrl.post_hashtags );
+router.get('/hashtags/:id', ctrl.get_hashtags_edit );
+router.post('/hashtags/:id', ctrl.post_hashtags_edit );
+
+router.get('/product_orders', ctrl.get_product_orders );
+router.post('/product_orders', ctrl.post_product_orders );
+router.get('/product_orders/:id', ctrl.get_product_orders_edit );
+router.post('/product_orders/:id', ctrl.post_product_orders_edit );
+
+router.get('/providers', ctrl.get_providers );
+router.post('/providers', ctrl.post_providers );
+router.get('/providers/:id', ctrl.get_providers_edit );
+router.post('/providers/:id', ctrl.post_providers_edit );
+
+router.get('/provider_handles', ctrl.get_provider_handles );
+router.post('/provider_handles', ctrl.post_provider_handles );
+router.get('/provider_handles/:id', ctrl.get_provider_handles_edit );
+router.post('/provider_handles/:id', ctrl.post_provider_handles_edit );
+
+module.exports = router;
+console.log('Routing success');
+
+//router.get('/products', ctrl.get_products );
+/*
 router.get('/products/write', ctrl.get_products_write );
 
 router.post('/products/write', ctrl.post_products_write );
@@ -29,14 +93,4 @@ router.get('/products/edit/:id', ctrl.get_products_edit );
 router.post('/products/edit/:id', ctrl.post_products_edit );
 
 router.get('/products/delete/:id', ctrl.get_products_delete );
-
-// future connect 
-router.get('/customers', ctrl.get_customers );
-router.post('/customers', ctrl.post_customers );
-router.get('/customers/:id', ctrl.get_customers_edit );
-router.post('/customers/:id', ctrl.post_customers_edit );
-router.get('/products', ctrl.get_products );
-router.get('/orders', ctrl.get_orders );
-router.get('/orders/detail/:id', ctrl.get_orders_detail );
-
-module.exports = router;
+*/
